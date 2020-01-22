da8e7027de54873cffa0acd76a2bc1a32347d71b
ambid17 <12086352+ambid17@users.noreply.github.com>
2020-01-21 19:01:18 -0600

    added player movement based off rigidbody

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,42 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
     [Header("Assign in editor")]
     public Tilemap tilemap; 
     public PlayerData playerData;
+    public float moveSpeed = 0.2f;
+    public float jumpSpeed = 10f;
+    public LayerMask groundLayerMask;
 
     private const int cellPositionOffset = 1;
 
+    private bool isGrounded;
+    public float groundCheckDistance = 5f;
+    
+    BoxCollider2D myCollider;
+    Rigidbody2D myRigidBody;
+
     void Start()
     {
+        myCollider = GetComponent<BoxCollider2D>();
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        CheckGrounded();
         CheckInput();
+    }
+
+    void CheckGrounded()
+    {
+        if(Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayerMask))
+        {
+            Debug.Log("grounded");
+            isGrounded = true;
+        }
+        else
+        {
+            Debug.Log("noop");
+            isGrounded = false;
+        }
     }
 
     void CheckInput()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-0.2f, 0, 0);
+            myRigidBody.MovePosition(transform.position + new Vector3(-moveSpeed, 0));
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(0.2f, 0, 0);
+            myRigidBody.MovePosition(transform.position + new Vector3(moveSpeed, 0));
         }
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && isGrounded)
         {
-            transform.position += new Vector3(0, 0.2f, 0);
+            myRigidBody.velocity = new Vector2(0, jumpSpeed);
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && isGrounded)
         {
+            // Add dig timer
             Dig();
         }
     }
