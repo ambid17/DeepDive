09653a140cf60da981ed9d6ef9496d77141ae5eb
ambid17 <12086352+ambid17@users.noreply.github.com>
2020-01-22 20:34:43 -0600

    working shop

--- Assets/Scripts/UI/BuyMenu/UpgradeUIManager.cs
+++ Assets/Scripts/UI/BuyMenu/UpgradeUIManager.cs
@@ -24,27 +24,27 @@ public class UpgradeUIManager : MonoBehaviour
             case UpgradeType.Drill:
                 numberOfUpgrades = playerData.upgradeStatus.drillUpgrade;
                 numberOfUpgradesText.text = numberOfUpgrades.ToString();
-                nextUpgradeCostText.text = "$" + upgrades.drillUpgrades.drillUpgrades[numberOfUpgrades + 1].ToString();
+                nextUpgradeCostText.text = "$" + upgrades.drillUpgrades.drillUpgrades[numberOfUpgrades + 1].cost.ToString();
                 break;
             case UpgradeType.Hull:
                 numberOfUpgrades = playerData.upgradeStatus.hullUpgrade;
                 numberOfUpgradesText.text = numberOfUpgrades.ToString();
-                nextUpgradeCostText.text = "$" + upgrades.hullUpgrades.hullUpgrades[numberOfUpgrades + 1].ToString();
+                nextUpgradeCostText.text = "$" + upgrades.hullUpgrades.hullUpgrades[numberOfUpgrades + 1].cost.ToString();
                 break;
             case UpgradeType.LightSize:
                 numberOfUpgrades = playerData.upgradeStatus.lightSizeUpgrade;
                 numberOfUpgradesText.text = numberOfUpgrades.ToString();
-                nextUpgradeCostText.text = "$" + upgrades.lightSizeUpgrades.lightSizeUpgrades[numberOfUpgrades + 1].ToString();
+                nextUpgradeCostText.text = "$" + upgrades.lightSizeUpgrades.lightSizeUpgrades[numberOfUpgrades + 1].cost.ToString();
                 break;
             case UpgradeType.LightBattery:
                 numberOfUpgrades = playerData.upgradeStatus.lightBatteryUpgrade;
                 numberOfUpgradesText.text = numberOfUpgrades.ToString();
-                nextUpgradeCostText.text = "$" + upgrades.lightBatteryUpgrades.lightBatteryUpgrades[numberOfUpgrades + 1].ToString();
+                nextUpgradeCostText.text = "$" + upgrades.lightBatteryUpgrades.lightBatteryUpgrades[numberOfUpgrades + 1].cost.ToString();
                 break;
             case UpgradeType.MiniMap:
                 numberOfUpgrades = playerData.upgradeStatus.minimapUpgrade;
                 numberOfUpgradesText.text = numberOfUpgrades.ToString();
-                nextUpgradeCostText.text = "$" + upgrades.minimapUpgrades.minimapUpgrades[numberOfUpgrades + 1].ToString();
+                nextUpgradeCostText.text = "$" + upgrades.minimapUpgrades.minimapUpgrades[numberOfUpgrades + 1].cost.ToString();
                 break;
         }
     }
